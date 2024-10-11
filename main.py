36de05edb325c68ea3d42ffd3c34a32768226f1e
tequila01-svg <onifadeayobami008@gmail.com>
2024-10-11 17:44:17 +0100

    date and time added

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ if num1%2==0:
 else:
     print("the number is odd")
     
+    
+ # date and time operations
+import datetime   
+ 
+current_time=datetime.datetime.now()
+print("the time in WAT is:", current_time)
+ 
+import calendar
+ 
+print("\n", calendar.calendar(2038))
